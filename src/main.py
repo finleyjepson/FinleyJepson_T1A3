322d444d6c6578e23a3856c331845a654aaec286
Finley <trinity.tringl@gmail.com>
2023-10-19 17:28:38 +1000

    Added main function selection page
    allows u to select login, register. & exit, all options do nothing for now.

---
+++ src/main.py
@@ -0,0 +1,25 @@
+def Login_Function():
+    pass
+
+def Register_Function():
+    pass
+
+def main():
+    while True:
+        print ("Welcome to the main menu")
+        print ("Please select from the following options:")
+        print ("1. Login")
+        print ("2. Register")
+        print ("3. Exit")
+        selection = input("Please enter your selection: ")
+        if selection == "1":
+            Login_Function()
+        elif selection == "2":
+            Register_Function()
+        elif selection == "3":
+            print ("Thank you for using the program")
+            break
+        else:
+            print ("Invalid selection, please try again")
+
+main()
