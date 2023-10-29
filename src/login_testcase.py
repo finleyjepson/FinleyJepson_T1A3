341c631d9460fbbfee54aa51e751062c4870e8eb
Finley <trinity.tringl@gmail.com>
2023-10-29 19:50:32 +1000

    added test units

--- src/login_testcase.py
+++ src/login_testcase.py
@@ -3,7 +3,17 @@ from unittest.mock import patch, Mock
 import hashlib
 from maintest import UserManager
 
+import unittest
+from unittest.mock import patch
+import hashlib
+from user_manager import UserManager
+
 class TestUserManager(unittest.TestCase):
+
+    # This class contains unit tests for the UserManager class.
+
+    # Test 1: Test successful login
+    # Test 2: Test invalid username login
 
     def setUp(self):
         # This method is run before each test
@@ -18,6 +28,13 @@ class TestUserManager(unittest.TestCase):
     @patch('builtins.input', side_effect=['testuser'])
     @patch('getpass.getpass', return_value='Test1234!')
     def test_successful_login(self, mock_getpass, mock_input):
+
+        # Test that a user can successfully log in with valid credentials.
+
+        # Test cases:
+        # 1. User enters correct username and password. Expected result: True.
+        # 2. User enters correct username but incorrect password. Expected result: False.
+
         # Insert a user to simulate the existing user in database
         enc_pass = 'Test1234!'.encode()
         hash_pass = hashlib.md5(enc_pass).hexdigest()
@@ -27,10 +44,29 @@ class TestUserManager(unittest.TestCase):
         result = self.user_manager.login('testuser', 'Test1234!')
         self.assertTrue(result)
 
+        result = self.user_manager.login('testuser', 'WrongPass')
+        self.assertFalse(result)
+
     @patch('builtins.input', side_effect=['invaliduser'])
     @patch('getpass.getpass', return_value='WrongPass')
     def test_invalid_username_login(self, mock_getpass, mock_input):
+
+        # Test that a user cannot log in with an invalid username.
+
+        # Test cases:
+        # 1. User enters an invalid username. Expected result: False.
+        # 2. User enters a valid username but incorrect password. Expected result: False.
+
         result = self.user_manager.login('invaliduser', 'WrongPass')
+        self.assertFalse(result)
+
+        # Insert a user to simulate the existing user in database
+        enc_pass = 'Test1234!'.encode()
+        hash_pass = hashlib.md5(enc_pass).hexdigest()
+        self.user_manager.cur.execute("INSERT OR IGNORE INTO credentials (username, password) VALUES (?, ?)", ('testuser', hash_pass))
+        self.user_manager.conn.commit()
+
+        result = self.user_manager.login('testuser', 'WrongPass')
         self.assertFalse(result)
 
 # If this module is the main module, run the tests
--- src/register_testcase.py
+++ src/register_testcase.py
@@ -1,25 +1,55 @@
 import unittest
 from unittest.mock import patch
-from maintemp import UserManager
+from user_manager import UserManager
 import hashlib
 
 class TestUserManager(unittest.TestCase):
 
+    # This class contains two test cases that check the functionality of the UserManager class.
+
+    # Test Case 1: test_successful_registration
+    # This test case checks if a user can be successfully registered with valid credentials.
+    # Test Cases:
+    # 1. User enters valid credentials and the registration is successful.
+    # 2. User enters invalid credentials and the registration fails.
+
+    # Test Case 2: test_existing_username_registration
+    # This test case checks if the registration fails when a user tries to register with an existing username.
+    # Test Cases:
+    # 1. User tries to register with an existing username and the registration fails.
+    # 2. User tries to register with a new username and the registration is successful.
+
+
     @patch('builtins.input', side_effect=['testuser', 'Test1234!', 'Test1234!'])
     @patch('getpass.getpass', side_effect=['Test1234!', 'Test1234!'])
     def test_successful_registration(self, mock_getpass, mock_input):
+
+        # This test case checks if a user can be successfully registered with valid credentials.
+
+        # Test Cases:
+        # 1. User enters valid credentials and the registration is successful.
+
+        # Create an instance of the UserManager class
         user_manager = UserManager()
         
         # Clear the test user if exists
         user_manager.cur.execute("DELETE FROM credentials WHERE username=?", ('testuser',))
         user_manager.conn.commit()
 
+        # Call the register function of the UserManager class
         result = user_manager.register('testuser', 'Test1234!', 'Test1234!')
         self.assertEqual(result, "You have registered successfully!")
 
     @patch('builtins.input', side_effect=['existinguser', 'Test1234!', 'Test1234!'])
     @patch('getpass.getpass', side_effect=['Test1234!', 'Test1234!'])
     def test_existing_username_registration(self, mock_getpass, mock_input):
+        # This test case checks if the registration fails when a user tries to register with an existing username.
+
+        # Test Cases:
+        # 1. User tries to register with an existing username and the registration fails.
+        # 2. User tries to register with a new username and the registration is successful.
+
+        # Create an instance of the UserManager class
         user_manager = UserManager()
         
         # Create a user to simulate existing user scenario
@@ -28,6 +58,7 @@ class TestUserManager(unittest.TestCase):
         user_manager.cur.execute("INSERT OR IGNORE INTO credentials (username, password) VALUES (?, ?)", ('existinguser', hash_pass))
         user_manager.conn.commit()
 
+        # Call the register function of the UserManager class
         result = user_manager.register('existinguser', 'Test1234!', 'Test1234!')
         self.assertEqual(result, "Username already exists. Please try again with a different username.")
 
